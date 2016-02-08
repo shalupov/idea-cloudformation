95bf0055169788b6f6c81a514c6754e24fc32581
Leonid Shalupov <leonid@shalupov.com>
2016-02-08 01:00:27 +0100

    Provide a way to work with other file name extensions #49

--- src/main/java/com/intellij/aws/cloudformation/CloudFormationFileType.java
+++ src/main/java/com/intellij/aws/cloudformation/CloudFormationFileType.java
@@ -2,16 +2,15 @@ package com.intellij.aws.cloudformation;
 
 import com.google.common.base.Charsets;
 import com.intellij.icons.AllIcons;
+import com.intellij.json.JsonFileType;
 import com.intellij.json.JsonLanguage;
+import com.intellij.openapi.fileTypes.FileTypeManager;
 import com.intellij.openapi.fileTypes.LanguageFileType;
 import com.intellij.openapi.fileTypes.ex.FileTypeIdentifiableByVirtualFile;
-import com.intellij.openapi.util.io.ByteSequence;
-import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileSystem;
 import com.intellij.openapi.vfs.newvfs.FileSystemInterface;
 import com.intellij.openapi.vfs.newvfs.impl.StubVirtualFile;
-import com.intellij.util.Processor;
 import org.apache.commons.lang.ArrayUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -58,7 +57,12 @@ public class CloudFormationFileType extends LanguageFileType implements FileType
 
   @Override
   public boolean isMyFileType(@NotNull VirtualFile file) {
-    return CloudFormationFileType.EXTENSION.equalsIgnoreCase(file.getExtension()) && detectFromContent(file);
+    String extension = file.getExtension();
+
+    return extension != null &&
+        (FileTypeManager.getInstance().getFileTypeByExtension(extension) == JsonFileType.INSTANCE ||
+            CloudFormationFileType.EXTENSION.equalsIgnoreCase(extension)) &&
+        detectFromContent(file);
   }
 
   private boolean detectFromContent(@NotNull VirtualFile file) {
